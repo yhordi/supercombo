3cc82ebf5726f51356d90fa0b82e58d17cccba37
sdickey <sean.dickey@live.com>
2014-09-15 15:56:52 -0700

    Added username validation for presence and test

--- supercombo/spec/models/user_spec.rb
+++ supercombo/spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
-describe "validations" do
-  it { should validate_presence_of :username }
+describe User do
+  context "validations" do
+    before :each do
+      @user = User.new
+      @user.username = "MrA'sBaseball"
+    end
+  it { should validate_presence_of (:username) }
+  end
 end
