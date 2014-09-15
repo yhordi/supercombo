283a71de700a16ee41a76e31747373576115c382
sdickey <sean.dickey@live.com>
2014-09-15 15:44:01 -0700

    Added user validation test

--- supercombo/spec/models/user_spec.rb
+++ supercombo/spec/models/user_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "validations" do
+  it { should validate_presence_of :username }
 end
--- supercombo/spec/spec_helper.rb
+++ supercombo/spec/spec_helper.rb
@@ -1,3 +1,3 @@
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment". __FILE__)
+require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/autorun'
