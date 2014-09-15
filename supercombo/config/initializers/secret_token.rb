42989e2b08b9f80f45938afcec5cb5cbee3e9753
sdickey <sean.dickey@live.com>
2014-09-15 14:58:45 -0700

    Added rails framework and user migration columns

---
+++ supercombo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Supercombo::Application.config.secret_key_base = 'b35258583490593bbd995cbb41b0182fbc6998f49d1b0c2e028531ee794bec107b293f32cd9197a6c8081596049ff61c8f67757d4b182fbc22f9b72ee9bd57a9'
---
+++ supercombo/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Supercombo::Application.config.session_store :cookie_store, key: '_supercombo_session'
