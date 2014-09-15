75e447205513c96c25e6f5cb9817148f1717b3fc
sdickey <sean.dickey@live.com>
2014-09-15 16:29:19 -0700

    Added password tests and validations

--- supercombo/app/models/user.rb
+++ supercombo/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 14 }
 end
--- supercombo/spec/models/user_spec.rb
+++ supercombo/spec/models/user_spec.rb
@@ -5,7 +5,18 @@ describe User do
     before :each do
       @user = User.new
       @user.username = "MrA'sBaseball"
+      @user.password = "IamMrBaseba11"
     end
-  it { should validate_presence_of (:username) }
+    context "username" do
+      it { should validate_presence_of (:username) }
+      it { should validate_uniqueness_of (:username) }
+    end
+    context "password" do
+      it { should validate_presence_of (:password) }
+      # it { should ensure_length_of (:password) }
+      it "should be longer than 6 characters" do
+        expect(@user.password.length).to be > 6
+      end
+    end
   end
 end
